4da3679abb3b8f6d11b8ca8af6ca355489b39562
nahump <28376747+onahump@users.noreply.github.com>
2024-12-18 14:16:10 -0600

    Update main.tf

--- terraform-gcp-storage-bucket/main.tf
+++ terraform-gcp-storage-bucket/main.tf
@@ -2,12 +2,11 @@ resource "google_storage_bucket" "bucket" {
   name          = var.bucket_name
   location      = var.region
   storage_class = var.storage_class
+  uniform_bucket_level_access = var.bucket_level_access
 
   versioning {
     enabled = var.enable_versioning
   }
-
-  uniform_bucket_level_access = var.bucket_level_access
 
   lifecycle_rule {
     condition {
